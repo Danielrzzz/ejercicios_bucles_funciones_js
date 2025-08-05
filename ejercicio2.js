1804ec608b6b5a7058201f4c0359b15c92e6fb7f
Danielrzzz <daniel.rodriguez2999@gmail.com>
2025-08-05 17:46:14 +0200

    ejercicios_2_3_4_5
    y comienzo del 6

---
+++ ejercicio2.js
@@ -0,0 +1,24 @@
+const movies = [
+  { title: 'The Matrix', releaseYear: 1999 },
+  { title: 'Star Wars: Episode IV – A New Hope', releaseYear: 1977 },
+  { title: 'Inception', releaseYear: 2010 },
+  { title: 'Jurassic Park', releaseYear: 1993 },
+  { title: 'The Shawshank Redemption', releaseYear: 1994 },
+  { title: 'Pulp Fiction', releaseYear: 1994 },
+  { title: 'Avatar', releaseYear: 2009 },
+  { title: 'The Dark Knight', releaseYear: 2008 },
+  { title: 'Fight Club', releaseYear: 1999 },
+  { title: 'Forrest Gump', releaseYear: 1994 }
+];
+const count = {
+    before2000: 0,
+    after2000: 0
+};
+for (let movie of movies){
+    if(movie.releaseYear <= 2000){
+        count.before2000++;
+    } else {
+        count.after2000++
+    }
+}
+ console.log(count);
---
+++ ejercicio4.js
@@ -0,0 +1,24 @@
+const cartoons = [
+  { name: 'Mickey Mouse', debut: 1928 },
+  { name: 'Bugs Bunny', debut: 1938 },
+  { name: 'SpongeBob SquarePants', debut: 1999 },
+  { name: 'Tom and Jerry', debut: 1940 },
+  { name: 'Scooby-Doo', debut: 1969 },
+  { name: 'The Flintstones', debut: 1960 },
+  { name: 'Batman: The Animated Series', debut: 1992 },
+  { name: 'The Simpsons', debut: 1989 },
+  { name: 'Pokémon', debut: 1997 },
+  { name: "Dexter's Laboratory", debut: 1996 }
+];
+
+
+let olderCartoon = cartoons[0];
+
+for (let i = 0;i < cartoons.length; i++) {
+    let cartoon = cartoons[i];
+    if(cartoon.debut < olderCartoon.debut){
+        olderCartoon = cartoon;
+    }
+}
+
+console.log(olderCartoon.name);   //es un objeto, por lo que tengo que llamar al name dentro del objeto, no estoy seguro si así vale igual el ejercicio.
